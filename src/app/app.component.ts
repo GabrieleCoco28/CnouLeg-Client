4a408cee9580def81f27f60a4424439f17e4b75c
gabri <gabrielecoco0@gmail.com>
2024-03-19 21:22:20 +0100

    addedd shortcut t for changing theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,28 @@ import { transition, trigger, useAnimation } from '@angular/animations';
 import { ViewportScroller } from '@angular/common';
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { Router, Scroll } from '@angular/router';
-import { fromLeftEasing, fromRightEasing, moveFromLeft, moveFromRight } from 'ngx-router-animations';
+import {
+  fromLeftEasing,
+  fromRightEasing,
+  moveFromLeft,
+  moveFromRight,
+} from 'ngx-router-animations';
 import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   public title = 'CnouLeg-Client';
+  constructor() {
+    addEventListener('keydown', (e: KeyboardEvent) => {
+      if (e.key === 't' || e.key === 'T') {
+        if (document.body.className.includes('light-theme'))
+          document.body.className = 'mat-typography dark-theme';
+        else document.body.className = 'mat-typography light-theme';
+      }
+    });
+  }
 }
